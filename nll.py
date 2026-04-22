3212f74380241339975a56ce22d206e39e92b1ca
Faizal Addi <faizal.addi@gmail.com>
2026-04-22 22:54:47 +0900

    Add nll.py file

---
+++ nll.py
@@ -0,0 +1,20 @@
+# Debug this code
+import math
+
+def nll(actual, predicted_proba):
+	'''return the negative loglikelihood. Return NLL (float)'''
+	N = len(actual)
+	sum_ = 0
+	for i in range(N):
+		sum_ += (actual[i] * math.log(predicted_proba[i]) + (1-actual[i]) * math.log(1 - predicted_proba[i]))
+	nll = -1/N * sum_
+	return nll
+  
+
+
+# Input
+
+
+res = nll(actual, predicted_proba)
+print(res)
+# Should be 0.16836656419122908
